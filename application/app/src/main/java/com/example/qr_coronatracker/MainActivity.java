233aa1017e767b09fbe2703570f25ae5e5044914
Ravi <rgbj60@gmail.com>
2020-04-29 21:14:43 -0700

    Summary
    first try implementing library, no idea if it works or if it is screwed up

--- application/app/src/main/java/com/example/qr_coronatracker/MainActivity.java
+++ application/app/src/main/java/com/example/qr_coronatracker/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.qr_coronatracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.logging.Logger;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private Object AppConstants;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Programmatically initialize the scanner view
+        mScannerView = new ZXingScannerView(this);
+        // Set the scanner view as the content view
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private ZXingScannerView mScannerView;
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register ourselves as a handler for scan results.
+        mScannerView.setResultHandler(this);
+        // Start camera on resume
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        // Prints scan results
+        Logger.getLogger("result", rawResult.getText());
+        // Prints the scan format (qrcode, pdf417 etc.)
+        Logger.getLogger("result", rawResult.getBarcodeFormat().toString());
+        //If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
+        Intent intent = new Intent();
+        intent.putExtra("1234", rawResult.getText());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
