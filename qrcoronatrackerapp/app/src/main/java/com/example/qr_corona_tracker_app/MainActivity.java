2ad2be62d3b77a4d27702e76e5ed93ba6b0411d3
Marco Paredes <maparede@ucsd.edu>
2020-05-11 01:01:31 -0700

    Created QR, needs testing

--- qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/MainActivity.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.qr_corona_tracker_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button scanBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanBtn = findViewById(R.id.scanBtn); // Set scanBtn to button on screen
+
+        // Generate an action for clicking button
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Scanner activity, read QR code
+                startActivity(new Intent(getApplicationContext(), Scanner.class));
+            }
+        });
     }
 }
---
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/Scanner.java
@@ -0,0 +1,105 @@
+/**
+ * Represents a QR Scanner
+ * <p>
+ *     Note: Need to add dependencies:
+ *              'implementation 'com.budiyev.android:code-scanner:2.1.0'' and
+ *              'implementation 'com.karumi:dexter:6.0.0''
+ *           under 'dependencies' in 'build.gradle; in order to build the project, then sync project
+ * </p>
+ * @author: Marco Paredes
+ * @version: 1.0.0
+ * @since 1.0
+ */
+
+package com.example.qr_corona_tracker_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class Scanner extends AppCompatActivity {
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+
+        // Create Scanner objects based on dependencies
+        scannView = findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this,scannView);
+        resultData = findViewById(R.id.resultQR);
+
+        // Used to get QR results, can access with Result result
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+                    }
+                });
+
+            }
+        });
+
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+
+    }
+
+    /**
+     * Asks user permission to access camera to scan QR
+     */
+    public void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(Scanner.this, "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+
+}
