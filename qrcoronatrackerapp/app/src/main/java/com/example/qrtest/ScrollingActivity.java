b296729acd3c248c09b1245e16d61cfd2b737872
Ravi <rgbj60@gmail.com>
2020-05-30 22:42:31 -0700

    Back button
    put in a button that can go back

--- qrcoronatrackerapp/app/src/main/java/com/example/qrtest/ScrollingActivity.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qrtest/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.example.qrtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,22 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class ScrollingActivity extends AppCompatActivity {
-
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View vv) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
     }
