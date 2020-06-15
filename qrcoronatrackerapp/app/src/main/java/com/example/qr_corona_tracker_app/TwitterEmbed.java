d46e8888388b17f85554681afb1342521a42bf74
Marco Paredes <maparede@ucsd.edu>
2020-06-14 21:26:33 -0700

    Created new Twitter class

--- qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterEmbed.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterEmbed.java
@@ -1,15 +1,16 @@
+package com.example.qr_corona_tracker_app;
+
 import android.os.Bundle;
+import android.view.Menu;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-import com.example.qr_corona_tracker_app.R;
-
-<a class="twitter-timeline" href="http://twitter.com/WHO" data-widget-id="394415351972114432">Tweets about "#WHO"</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-
-public class MainActivity extends ActionBarActivity {
+public class TwitterEmbed extends ActionBarActivity {
 
     public static final String TAG = "TimeLineActivity";
 
@@ -48,22 +49,3 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 }
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-        xmlns:tools="http://schemas.android.com/tools"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:paddingBottom="@dimen/activity_vertical_margin"
-        android:paddingLeft="@dimen/activity_horizontal_margin"
-        android:paddingRight="@dimen/activity_horizontal_margin"
-        android:paddingTop="@dimen/activity_vertical_margin"
-        tools:context=".Latest Coronavirus News" >
-
-<WebView
-        android:id="@+id/timeline_webview"
-                android:layout_width="fill_parent"
-                android:layout_height="fill_parent"/>
-
-</RelativeLayout>
-
-<uses-permission android:name="android.permission.INTERNET" />
-//this has to go into activity?
---
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
@@ -0,0 +1,12 @@
+package com.example.qr_corona_tracker_app;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Toolbar;
+
+public class TwitterHandle extends Toolbar {
+
+}
