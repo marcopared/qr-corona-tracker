9d0c83154775dfc04f06ab6755e1c861c399f0ba
Marco Paredes <maparede@ucsd.edu>
2020-06-15 23:51:57 -0700

    Updated CircularProgressBar classs

--- qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
@@ -22,6 +22,7 @@ public class TwitterHandle extends AppCompatActivity {
     private static final String baseURl = "http://twitter.com";
     private static final String widgetInfo = "<a class=\"twitter-timeline\" href=\"http://twitter.com/WHO\" data-widget-id=\"394415351972114432\">Tweets about \"#WHO\"</a> " +
             "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\"://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +35,20 @@ public class TwitterHandle extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadDataWithBaseURL(baseURl, widgetInfo, "text/html", "UTF-8",null);
     }
+
     private void load_background_color () {
         WebView webView = (WebView) findViewById(R.id.timeline_webview);
         webView.setBackgroundColor(0);
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return true;
     }
 }
-
