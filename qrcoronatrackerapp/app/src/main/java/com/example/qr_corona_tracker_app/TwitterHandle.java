26796f6d1c542c5015c04bf4986aab8434049a12
Philip <philip.emmanue@gmail.com>
2020-06-20 23:58:32 -0700

    fixed up twitter handle so hopefully it works, also added comments

--- qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
@@ -15,10 +15,9 @@ import android.webkit.WebView;
 import androidx.appcompat.app.AppCompatActivity;
 
     public class TwitterHandle extends AppCompatActivity {
-        public static final String TAG = "TimeLineActivity";
         private static final String baseURl = "http://twitter.com";
-        private static final String widgetInfo = "<a class=\"twitter-timeline\" href=\"http://twitter.com/WHO\" data-widget-id=\"394415351972114432\">Tweets about \"#WHO\"</a> " +
-                "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\"://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>";
+        private static final String widgetInfo = "https://twitter.com/who";
+        //establishes parameters (??) that will be used later for loadDataWithBaseURL
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -27,14 +26,18 @@ import androidx.appcompat.app.AppCompatActivity;
             load_background_color();
 
             WebView webView = (WebView) findViewById(R.id.timeline_webview);
+            //method call which locates WebView in layout file
             webView.getSettings().setDomStorageEnabled(true);
+            //need to use local storage to store the website when it is called
             webView.getSettings().setJavaScriptEnabled(true);
+            //JavaScript is disabled by default so we need to enable it for it to work in the app
             webView.loadDataWithBaseURL(baseURl, widgetInfo, "text/html", "UTF-8",null);
+            //links on the page need to know where they came from if clicked on, so this makes it so the code knows youre coming from twitter.com/who
         }
         private void load_background_color () {
             WebView webView = (WebView) findViewById(R.id.timeline_webview);
             webView.setBackgroundColor(0);
-
+// class that establishes background of webview as transparent
         }
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
