3bc73493ef94fa601a1b18901e445b08e4cc3bed
Philip <philip.emmanue@gmail.com>
2020-06-21 17:30:06 -0700

    Finally it is working

--- qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
+++ qrcoronatrackerapp/app/src/main/java/com/example/qr_corona_tracker_app/TwitterHandle.java
@@ -8,43 +8,34 @@
  */
 package com.example.qr_corona_tracker_app;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebViewClient;
 
-    public class TwitterHandle extends AppCompatActivity {
-        private static final String baseURl = "http://twitter.com";
-        private static final String widgetInfo = "https://twitter.com/who";
-        //establishes parameters (??) that will be used later for loadDataWithBaseURL
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.twitter_handle);
+public class TwitterHandle extends Activity {
+    private WebView webview;
 
-            load_background_color();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.twitter_handle);
+        //where we want this code to display (specific xml file)
 
-            WebView webView = (WebView) findViewById(R.id.timeline_webview);
-            //method call which locates WebView in layout file
-            webView.getSettings().setDomStorageEnabled(true);
-            //need to use local storage to store the website when it is called
-            webView.getSettings().setJavaScriptEnabled(true);
-            //JavaScript is disabled by default so we need to enable it for it to work in the app
-            webView.loadDataWithBaseURL(baseURl, widgetInfo, "text/html", "UTF-8",null);
-            //links on the page need to know where they came from if clicked on, so this makes it so the code knows youre coming from twitter.com/who
-        }
-        private void load_background_color () {
-            WebView webView = (WebView) findViewById(R.id.timeline_webview);
-            webView.setBackgroundColor(0);
-// class that establishes background of webview as transparent
-        }
+        String url = "http://twitter.com/WHO";
+        //what webapge we want to go to
+
+        webview = (WebView) findViewById(R.id.myWebView);
+        webview.setWebViewClient(new MyWebViewClient());
+        //makes it so webpage is viewed in app not in browser
+        webview.getSettings().setJavaScriptEnabled(true);
+        //enables Java
+        webview.loadUrl(url);
+        //loads the url
+    }
+    private class MyWebViewClient extends WebViewClient {
         @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            return true;
-        }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            return true;
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            return false;
         }
     }
+}
